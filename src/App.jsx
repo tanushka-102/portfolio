31b6f675c5429688c972a53fd62198a2cc909774
Tanushka Verma <124056303+tanushka-102@users.noreply.github.com>
2026-05-11 23:25:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Projects from '@/components/Projects';
-import DataVisualization from '@/components/DataVisualization';
 import Education from '@/components/Education';
 import Skills from '@/components/Skills';
 import Contact from '@/components/Contact';
